a58803b575f9deb2e60fe0f9b366f691ba3ee178
Nguyen Toan Thang <toanthang15082002@gmail.com>
2025-12-14 18:16:07 +0700

    feat: update css, add question historic, science

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function checkVictory() {
 
 async function fetchQuestions() {
   try {
-    const response = await fetch('./technology_grade_fifth.json'); // FILE CÂU HỎI Ở ĐÂY
+    const response = await fetch('./science_grade_fifth.json'); // FILE CÂU HỎI Ở ĐÂY
     if (!response.ok) throw new Error(`HTTP ${response.status}`);
     questions = await response.json();
   } catch (err) {
